87c2b9de872d23ec1f3d38c345b18d2754e55afe
Nivas <jawaharsrinivs77@gmail.com>
2023-01-18 17:37:45 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -25,10 +25,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/seed', seedRouter);
-app.use(
-  'https://wild-rose-jellyfish-veil.cyclic.app/api/products',
-  productRouter
-);
+app.use('/api/products', productRouter);
 // app.use(core());
 
 app.use('/api/users', userRouter);
